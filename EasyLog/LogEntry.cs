0f3cbe4aacb1ff763891c72a05369c594ac177c4
Ginitho <nicokanian@gmail.com>
2026-04-16 09:25:40 +0200

    Consol version

---
+++ EasyLog/LogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EasyLog
+{
+    public class LogEntry
+    {
+        public string Timestamp { get; set; }
+        public string BackupName { get; set; }
+        public string SourceFile { get; set; }
+        public string TargetFile { get; set; }
+        public long FileSize { get; set; }
+        public long TransferTime { get; set; }
+
+        public LogEntry()
+        {
+            Timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
---
+++ EasyLog/StateEntry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EasyLog
+{
+    public class StateEntry
+    {
+        public string Name { get; set; }
+        public string Timestamp { get; set; }
+        public string State { get; set; }
+        public int TotalFilesToCopy { get; set; }
+        public long TotalFilesSize { get; set; }
+        public int NbFilesLeftToDo { get; set; }
+        public int Progression { get; set; }
+        public string CurrentSourceFile { get; set; }
+        public string CurrentTargetFile { get; set; }
+
+        public StateEntry()
+        {
+            Timestamp = DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss");
+        }
+    }
+}
